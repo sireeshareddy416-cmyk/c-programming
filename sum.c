486653296c377a8023e554e36cdc8dda401a4be6
sireeshareddy416-cmyk <sireeshareddy416@gmail.com>
2025-11-27 14:34:31 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sum;
+printf("\n enter the values of a and b is:");
+scanf("%d,%d",&a,&b);
+sum=a+b;
+printf("the value of a is %d and value of b is %d\n"a,b,sum);
+return (0);
+}
