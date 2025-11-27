a4ba24187437147e66d7e252786da76147f39d88
sireeshareddy416-cmyk <sireeshareddy416@gmail.com>
2025-11-27 15:34:25 +0530

    Create lower to upper case.c

---
+++ lower to upper case.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("\n enter any character in lower case:");
+    scanf("%c",ch);
+    printf("\n the typed character in upper case %c",ch);
+}
