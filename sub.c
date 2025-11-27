23b8d6aff29ae8006dbf28f8940dd33a128da9a8
sireeshareddy416-cmyk <sireeshareddy416@gmail.com>
2025-11-27 15:07:51 +0530

    Create sub.c

---
+++ sub.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,sub;
+    printf("\n enter the values of a and b is:");
+    scanf("%d,%d",&a,&b);
+    sub=a-b;
+    printf("the value of a is %d and the value of b is %d and sub is %d\n",a,b,sub);
+    return (0);
+}
